1650c7660f913b1c10e8f31d4f982aeaeeaa7f29
Ray Rodrigues <ray219.rodrigues@gmail.com>
2023-11-28 22:00:19 -0300

    AtualizaçãoCarro

---
+++ aplicação/src/af/a.java
@@ -0,0 +1,22 @@
+package af;
+
+import teste.javinha;
+
+public class a {
+
+
+public static void main(String[] args) {
+    
+    
+javinha testagem= new javinha();
+
+testagem.valor=10;
+
+    System.out.println(testagem.getValor());
+
+
+}
+
+
+
+}
---
+++ aplicação/src/teste/javinha.java
@@ -0,0 +1,18 @@
+package teste;
+
+public class javinha {
+    public int valor;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+
+
+
+
+}
